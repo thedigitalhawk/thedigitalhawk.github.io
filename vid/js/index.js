cf5a8d7f0c7d6612903351cd12b2e4aeef1d003b
thedigitalhawk <nightcry7@gmail.com>
2015-04-10 14:09:29 -0400

    Videotest

---
+++ vid/js/index.js
@@ -0,0 +1,29 @@
+var minWidth = 300;
+var videoWidth;
+var videoHeight;
+
+$(function() {
+
+    videoWidth = parseInt($('video').attr('width'));
+    videoHeight = parseInt($('video').attr('height'));
+
+    $(window).resize(function () { resizeVideo(); });
+    $(window).trigger('resize');
+
+});
+
+function resizeVideo() {
+    $('#videoContainer').width($('.videoBg').width());
+    $('#videoContainer').height($('.videoBg').height());
+    var sVideoWidth= $('.videoBg').width() / videoWidth;
+    var sVideoHeight = $('.videoBg').height() / videoHeight;
+    var videoFinalScale = sVideoWidth> sVideoHeight ? sVideoWidth: sVideoHeight;
+    if (videoFinalScale * videoWidth < minWidth) {videoFinalScale = minWidth / videoWidth;};
+
+    $('video').width(videoFinalScale * videoWidth);
+    $('video').height(videoFinalScale * videoHeight);
+
+    $('#videoContainer').scrollLeft(($('video').width() - $('.videoBg').width()) / 2);
+    $('#videoContainer').scrollTop(($('video').height() - $('.videoBg').height()) / 2);
+
+};
