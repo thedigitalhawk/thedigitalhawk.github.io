9659731fc6713d1e1311fed2f2c489c0d84f628c
Brett Sauers <thedigitalhawk@gmail.com>
2016-07-22 22:57:51 -0400

    site v3

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+      if($element.hasClass('odd')){
+        $element.addClass('slideInLeft');
+      }else{
+        $element.addClass('slideInRight');
+      }
+
+    } else {
+      $element.removeClass('in-view');
+      //$element.removeClass('zoomIn');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
